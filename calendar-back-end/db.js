21976036481b11cff6d3a5a04225538bf56711c9
Bhargav <bhargav@Bhargavs-MacBook-Air.local>
2024-11-27 21:11:09 +0530

    update db.js

--- calendar-back-end/db.js
+++ calendar-back-end/db.js
@@ -1,15 +1,15 @@
 const { Sequelize, QueryTypes } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-      host: process.env.DB_HOST || 'localhost',
-      dialect: 'postgres',
-      logging: console.log,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // This skips verification but should be handled properly for production
+    }
   }
-);
+});
+
 
 sequelize.authenticate()
   .then(() => {
