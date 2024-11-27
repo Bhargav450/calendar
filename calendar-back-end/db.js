13c075b19508ff77650de910b5ea11180f1aeefa
Bhargav <bhargav@Bhargavs-MacBook-Air.local>
2024-11-27 20:55:35 +0530

    first commit

--- calendar-back-end/db.js
+++ calendar-back-end/db.js
@@ -1,25 +1,23 @@
-require('dotenv').config({ path: require('path').resolve(__dirname, '.env') }); // Load .env from the root of the project
-
-
 const { Sequelize, QueryTypes } = require('sequelize');
 
-const dbUrl = process.env.DATABASE_URL || `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 const sequelize = new Sequelize(
-    dbUrl,
-    {
-        host: process.env.DB_HOST || 'localhost',
-        dialect: 'postgres',
-        logging: console.log,
-    }
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+      host: process.env.DB_HOST || 'localhost',
+      dialect: 'postgres',
+      logging: console.log,
+  }
 );
 
-// Check database connection status
 sequelize.authenticate()
   .then(() => {
-    console.log('Database connection has been established successfully.');
+    console.log('Database connection established successfully.');
   })
   .catch(err => {
     console.error('Unable to connect to the database:', err);
+    // Consider additional error handling here, such as retrying or notifying
   });
 
 module.exports = { sequelize, QueryTypes };
